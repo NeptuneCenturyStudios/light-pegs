4651327abd490924f1ae30ae9b028f3c9c234cd3
Eric Butler <e.c.butler@outlook.com>
2022-12-13 23:58:31 -0500

    Some expirements

---
+++ src/components/PegBoard.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="peg-board">
+
+        <template v-for="(hole, index) in holes" :key="index">
+            <div class="hole">
+            {{ hole }}
+            </div>
+        </template>
+        
+
+    </div>
+</template>
+
+<script>
+const MAX_HOLES = 1735;
+
+
+export default {
+    name: "PegBoard",
+    data() {
+        return {
+            holes: Array.from(Array(MAX_HOLES))
+        };
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<style lang="scss">
+.peg-board {
+    display: flex;
+flex-wrap: wrap;
+}
+
+.peg-board .hole {
+    background-color: #000;
+    border-radius: 100%;
+    width: 1rem;
+    height: 1rem;
+}
+</style>
