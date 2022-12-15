9b48744e84ab29c8bb792724289f18cf43e970a4
Eric Butler <e.c.butler@outlook.com>
2022-12-15 16:19:33 -0500

    Experiments

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <PegBoard></PegBoard>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PegBoard from '@/components/PegBoard.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    PegBoard
   }
 }
 </script>
